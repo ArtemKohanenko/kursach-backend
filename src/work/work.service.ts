7873ebd24bea17a7edf35e59f14bd6cbeb9539e8
Dokanin-ssh <sashadokanin@mail.ru>
2024-06-03 21:01:49 +0600

    fix my Work DTO & make findWorkByStatus

--- src/work/work.service.ts
+++ src/work/work.service.ts
@@ -5,6 +5,7 @@ import { Repository } from 'typeorm';
 import { Work } from './work.entity';
 import { TaskService } from 'src/task/task.service';
 import { StudentService } from 'src/student/student.service';
+import { WorkStatus } from './type/WorkStatusEnum';
 
 @Injectable()
 export class WorkService {
